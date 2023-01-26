28bdb552eb6e449b6ffb029c1ab5fa6ebc59ddb0
Santiago Baron Zuleta <santiagobazul@hotmail.com>
2023-01-26 12:45:28 -0500

    [Eventos en React] Manejando eventos con botones

--- counter-app/src/CounterApp.jsx
+++ counter-app/src/CounterApp.jsx
@@ -1,10 +1,19 @@
 import PropTypes from 'prop-types'
 
 export const CounterApp = ({ value }) => {
+    
+    const handleAdd = () => {
+        console.log('+1')
+    }
+    
     return (
         <>
             <h1>CounterApp</h1>
             <h2>{ value }</h2>
+
+            <button onClick={ handleAdd }>
+                +1
+            </button>
         </>
     )
 }

e2e1a28cb07ffb9541ce19a8a237953db8af76cf
Santiago Baron Zuleta <santiagobazul@hotmail.com>
2023-01-26 12:57:49 -0500

    [Hooks] useState (Boton con funcion, Hook, funcion hook llamada en funcion de boton)

--- counter-app/src/CounterApp.jsx
+++ counter-app/src/CounterApp.jsx
@@ -1,15 +1,18 @@
+import { useState } from 'react';
 import PropTypes from 'prop-types'
 
 export const CounterApp = ({ value }) => {
+
+    const [ counter, setCounter ] = useState( value )
     
     const handleAdd = () => {
-        console.log('+1')
+        setCounter(counter + 1)
     }
     
     return (
         <>
             <h1>CounterApp</h1>
-            <h2>{ value }</h2>
+            <h2>{ counter }</h2>
 
             <button onClick={ handleAdd }>
                 +1

528211301be0f1c37eaf0af3268dae8242a1fc07
Santiago Baron Zuleta <santiagobazul@hotmail.com>
2023-01-25 22:37:58 -0500

    [Usando fragment] Devolver elemento padre

---
+++ counter-app/src/FirstApp.jsx
@@ -0,0 +1,8 @@
+export const FirstApp = () => {
+    return(
+        <>
+            <h1>Santiago</h1>
+            <p>Soy un parrafo</p>
+        </>
+    )
+}
--- counter-app/src/main.jsx
+++ counter-app/src/main.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { FirstApp } from './FirstApp';
 import { HelloWorldApp } from './HelloWorldApp';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <HelloWorldApp></HelloWorldApp>
+        <FirstApp></FirstApp>
     </React.StrictMode>
 )

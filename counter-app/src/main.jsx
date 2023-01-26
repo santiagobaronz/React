e6a9a20642befbf328dad5038074056ffdd87f14
Santiago Baron Zuleta <santiagobazul@hotmail.com>
2023-01-25 22:17:55 -0500

    [Commit] Hola mundo

---
+++ counter-app/src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+function App() {
+    return (<h1>Hola mundo!!!</h1>);
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <App></App>
+    </React.StrictMode>
+)

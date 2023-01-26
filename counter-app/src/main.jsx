9e0f481d678966418592fe0f6583bea96aa13168
Santiago Baron Zuleta <santiagobazul@hotmail.com>
2023-01-25 22:28:55 -0500

    [Primer componente]

--- counter-app/src/main.jsx
+++ counter-app/src/main.jsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
-function App() {
-    return (<h1>Hola mundo!!!</h1>);
-}
+import { HelloWorldApp } from './HelloWorldApp';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <App></App>
+        <HelloWorldApp></HelloWorldApp>
     </React.StrictMode>
 )

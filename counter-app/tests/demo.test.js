6d3c28b014ee4a0238d205f8cc8a564309d30725
Santiago Baron Zuleta <santiagobazul@hotmail.com>
2023-01-26 18:54:27 -0500

    [Babel y test] Se añade Babel para evitar problemas

--- counter-app/tests/demo.test.js
+++ counter-app/tests/demo.test.js
@@ -1,8 +1,19 @@
+describe('Pruebas en el archivo de demo', () => {
 
-test('Esta prueba no debe de fallar', () => {
+    // Se debe hacer un solo describe por archivo de prueba
+    // Se pueden hacer tantos test por archivo como se quiera dentro del describe
 
-    if( 1 === 0 ){
-        throw new Error('No puede dividir entre 0')
-    }
+    test('Esta prueba no debe de fallar', () => {
 
+        // 1. Inicializacion
+        const message1 = 'Hola Mundo';
+    
+        // 2. Estimulo
+        const message2 = message1.trim();
+    
+        // 3. Observamos el comportamiento... esperado
+        expect(message1).toBe(message2)
+    
+    })
 })
+

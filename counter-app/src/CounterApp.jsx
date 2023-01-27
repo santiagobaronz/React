184a40bca3c257a24dfe9dda9736550dd3ce6be9
Santiago Baron Zuleta <santiagobazul@hotmail.com>
2023-01-26 22:16:31 -0500

    [Pruebas con click] fireEvent y aria-label con getByRole y name

--- counter-app/src/CounterApp.jsx
+++ counter-app/src/CounterApp.jsx
@@ -16,7 +16,7 @@ export const CounterApp = ({ value }) => {
 
             <button onClick={ handleAdd }>+1</button>
             <button onClick={ handleSustract }>-1</button>
-            <button onClick={ handleReset }>Reset</button>
+            <button aria-label='btn-reset' onClick={ handleReset }>Reset</button>
         </>
     )
 }
--- counter-app/tests/CounterApp.test.jsx
+++ counter-app/tests/CounterApp.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react"
+import { render, screen, fireEvent } from "@testing-library/react"
 import { CounterApp } from "../src/CounterApp";
 
 describe('Pruebas con el <CounterApp />', () => { 
@@ -13,6 +13,27 @@ describe('Pruebas con el <CounterApp />', () => {
      test('Debe mostrar el valor inicial de 100', () => { 
         render(<CounterApp value={100}/>)
         expect(screen.getByText(100)).toBeTruthy()
+     })
+
+     test('Debe de incrementar con el boton +1', () => { 
+        render(<CounterApp value={value}/>)
+        fireEvent.click(screen.getByText('+1'))
+        expect(screen.getByText(value + 1)).toBeTruthy()
+     })
+
+     test('Debe de decrementar con el boton -1', () => { 
+        render(<CounterApp value={value}/>)
+        fireEvent.click(screen.getByText('-1'))
+        expect(screen.getByText(value - 1)).toBeTruthy()
+     })
+
+     test('Debe de resetear con el boton Reset', () => { 
+        render(<CounterApp value={value}/>)
+        fireEvent.click(screen.getByText('+1'))
+        fireEvent.click(screen.getByText('+1'))
+        fireEvent.click(screen.getByText('+1'))
+        fireEvent.click(screen.getByRole('button', {name: 'btn-reset'}))
+        expect(screen.getByText(value)).toBeTruthy()
      })
 
 
